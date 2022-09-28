78c32430cf7db07cf5d78bbafe803a2af7b1443b
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-27 23:46:02 -0300

    Classe para realizar a chamada do web-serive foi criada

---
+++ client-service/src/br/com/alura/Client.java
@@ -0,0 +1,19 @@
+package br.com.alura;
+
+import java.net.http.HttpRequest;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+//		HttpRequest request = requestFactory.buildPostRequest(new genericUrl("https://picasaweb.google.com/data/feed/api/user/"+userId+"/"+albumId+"/default"), content);
+//		GoogleHeaders headers = new GoogleHeaders();
+//		String fileName = "Whatever...";
+//		headers.setSlugFromFileName(fileName);
+//		headers.setAuthorization("OAuth " + accessToken);
+//		request.setHeaders(headers);
+//		request.execute().ignore();
+
+	}
+
+}
